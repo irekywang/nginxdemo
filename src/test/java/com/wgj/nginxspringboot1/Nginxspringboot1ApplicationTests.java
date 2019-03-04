0dfe2181c076378f88a0f0eacb162b68d5591c4d
wangguangjun <ireky.wang@transn.com>
2019-03-04 16:46:01 +0800

    fir

---
+++ src/test/java/com/wgj/nginxspringboot1/Nginxspringboot1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wgj.nginxspringboot1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Nginxspringboot1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
